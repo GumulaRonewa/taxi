01b9ae15199907327a43c7d3e00b5ca482f03f11
GumulaRonewa <44128309+GumulaRonewa@users.noreply.github.com>
2021-08-12 04:53:36 +0200

    Update App.js

--- App.js
+++ App.js
@@ -8,7 +8,6 @@ require('dotenv/config');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
 app.use(express.json())
 app.use(parser.json())
 var locationRouter = require('./routes/routes');
