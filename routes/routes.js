e21ed3e0c8dc46f1fbcf7194b9fe833472787bc7
GumulaRonewa <44128309+GumulaRonewa@users.noreply.github.com>
2021-08-12 05:06:36 +0200

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -13,7 +13,7 @@ router.route('/add').post((req, res) => {
 });
 
 router.route('/search').post((req, res) => {
-  routes.find({from:req.body.From})
+  routes.find({Province:"WC")
       .then(route => res.json(route))
       .catch(err => console.log("Error")); 
 });
