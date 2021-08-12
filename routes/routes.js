4e4bad38522d0087b2601fd4571b1e15a187217f
GumulaRonewa <44128309+GumulaRonewa@users.noreply.github.com>
2021-08-12 02:25:46 +0200

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -3,12 +3,12 @@ let routes = require('../models/routes');
 router.route('/add').post((req, res) => {
   const Province = req.body.Province;
   const City=req.body.City;
-  const route=req.body.route;
+  const from=req.body.from;
+  const to=req.body.to;
   const price=req.body.price;
-  const newpost = new routes({Province,City,route,price});
-  console.log(req.body)
-  newpost.save()
-    .then(() => res.json("error"))
+  const newroute = new routes({Province,City,from,to,price});
+  newroute.save()
+    .then(() => res.status(200).json("added"))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
